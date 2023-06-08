e327a687cfe6d271ccc941c9cb149fa8e3e37bb7
Tom MBOYA <tommboya044@gmail.com>
2023-06-08 12:55:53 +0300

    1st commit

--- calc/src/main/java/org/example/Main.java
+++ calc/src/main/java/org/example/Main.java
@@ -6,5 +6,16 @@ public class Main {
         // to allow the program to read input from the user
             Scanner scanner = new Scanner(System.in);
 
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        double num1 = Double.parseDouble(scanner.nextLine());
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+        double num2 = Double.parseDouble(scanner.nextLine());
+
+        // Prompt the user to enter the operator
+        System.out.print("Enter the operator (+, -, *, /, %): ");
+        String operator = scanner.nextLine();
         }
 }
